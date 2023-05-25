020e40d4b8315b6afbf0eae45219b55282438490
tyler davis <tyler.davis432@gmail.com>
2023-05-24 23:14:35 -0400

    finished work

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+           
+            string dogName = "jack";
+            int age = 2;
+            char middleName = 'j';
+            bool nameIsjack = true;
+            double currentWeight = 66.3;
+            decimal currentPrice = 1200.73m;
+
+            Console.WriteLine($"I love {dogName}, he weighs {currentWeight}, he is {age}, and he's Pricey coming in at {currentPrice}. {dogName}'s real name is {nameIsjack}, he also has my middle name {middleName}.");
+
         }
     }
 }
